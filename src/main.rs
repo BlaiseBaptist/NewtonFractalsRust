623e02332f69f822e2b63d9909405e0327780e7c
BlaiseBaptist <bbaptist@thegraysonschool.org>
2024-03-01 14:22:49 -0500

    lots of upates

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use image::RgbImage;
 use rand::prelude::*;
-use std::ops;
 use rayon::iter::ParallelIterator;
-#[derive(Debug, Copy, Clone, PartialEq)]
+use std::ops;
+use std::fmt::Write;
+#[derive(Copy, Clone, PartialEq)]
 #[allow(non_camel_case_types)]
 struct c64 {
     r: f64,
     i: f64,
 }
-
 impl c64 {
     fn new(r: f64, i: f64) -> c64 {
         c64 { r, i }
@@ -30,6 +30,14 @@ impl From<i32> for c64 {
         }
     }
 }
+impl std::fmt::Display for c64 {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        if self.i > 0.0 {
+            return write!(f, "({:.2}+{:.2}i)", self.r, self.i);
+        }
+        write!(f, "({:.2}{:.2}i)", self.r, self.i)
+    }
+}
 impl ops::Mul<&c64> for &c64 {
     type Output = c64;
     fn mul(self, other: &c64) -> c64 {
@@ -39,7 +47,6 @@ impl ops::Mul<&c64> for &c64 {
         }
     }
 }
-
 impl ops::Add<&c64> for &c64 {
     type Output = c64;
     fn add(self, other: &c64) -> c64 {
@@ -63,8 +70,8 @@ impl ops::Div<&c64> for &c64 {
     type Output = c64;
     fn div(self, other: &c64) -> c64 {
         c64 {
-            r: (self.r * other.r + self.i * other.i) / (other.r.powi(2) + other.i.powi(2)),
-            i: (self.i * other.r - self.r * other.i) / (other.r.powi(2) + other.i.powi(2)),
+            r: (self.r * other.r + self.i * other.i) / (other.r * other.r + other.i * other.i),
+            i: (self.i * other.r - self.r * other.i) / (other.r * other.r + other.i * other.i),
         }
     }
 }
@@ -74,7 +81,6 @@ fn f(x: c64, roots: &[c64]) -> c64 {
         .iter()
         .fold(c64::new(1.0, 0.0), |acc, r| &acc * &(&x - r))
 }
-
 fn f_prime(x: c64, roots: &[c64]) -> c64 {
     let len = roots.len();
     if len == 1 {
@@ -82,12 +88,8 @@ fn f_prime(x: c64, roots: &[c64]) -> c64 {
     }
     let left = &roots[0..len / 2];
     let right = &roots[len / 2..];
-    &(&f(x, &left) * &f_prime(x, &right)) + &(&f_prime(x, &left) * &f(x, &right))
+    &(&f(x, left) * &f_prime(x, right)) + &(&f_prime(x, left) * &f(x, right))
 }
-
-type Range = [f64; 2];
-type Grid = Vec<Vec<c64>>;
-
 fn newton_iter(mut point: c64, roots: &[c64]) -> Option<usize> {
     for _ in 0..100 {
         point = &point - &(&f(point, roots) / &f_prime(point, roots));
@@ -100,42 +102,44 @@ fn newton_iter(mut point: c64, roots: &[c64]) -> Option<usize> {
     }
     None
 }
-
-fn gen_cplx_grid(xrange: &Range, nx: usize, yrange: &Range, ny: usize) -> Grid {
-    let mut data = vec![vec![c64::new(0.0, 0.0); ny]; nx];
-    let x_sep = (xrange[1] - xrange[0]) / (nx) as f64;
-    let y_sep = (yrange[1] - yrange[0]) / (ny) as f64;
-    for x in 0..nx {
-        for y in 0..ny {
-            data[x][y] = c64::new(
-                (x as f64 * x_sep) + xrange[0],
-                (y as f64 * y_sep) + yrange[0],
-            );
-        }
-    }
-    data
-}
-
-fn main() {
-    const LEN: usize = 50;
+fn make_random_im(nx: u32, ny: u32) {
+    let y_sep = 1.0 / ny as f64;
+    let x_sep = 1.0 / nx as f64;
+    let x_start = -0.5;
+    let y_start = -0.5;
+    const LEN: usize = 4;
     let roots: [c64; LEN] =
         core::array::from_fn(|_| c64::new(random::<f64>() - 0.5, random::<f64>() - 0.5));
-    let ny = 1000;
-    let nx = 1000;
     let colors: [[u8; 3]; LEN] =
         core::array::from_fn(|_| [random::<u8>(), random::<u8>(), random::<u8>()]);
     let mut imgbuf: RgbImage = image::ImageBuffer::new(nx, ny);
-    let grid = gen_cplx_grid(&[-1.0, 1.0], nx as usize, &[-1.0, 1.0], ny as usize);
-    imgbuf.par_enumerate_pixels_mut().for_each(|(x,y,pixel)| 
-        *pixel = image::Rgb(match newton_iter(grid[x as usize][y as usize], &roots) {
-            Some(i) => colors[i],
-            None => [0, 0, 0],
-        })
-    );
-	//TODO make the file names based on the roots
-    imgbuf.save("fractal4.png").unwrap();
+    imgbuf.par_enumerate_pixels_mut().for_each(|(x, y, pixel)| {
+        *pixel = image::Rgb(
+            match newton_iter(
+                c64::new((x as f64 * x_sep) + x_start, (y as f64 * y_sep) + y_start),
+                &roots,
+            ) {
+                Some(i) => colors[i],
+                None => [0, 0, 0],
+            },
+        )
+    });
+	let mut path =String::from("fractals/");
+	for root in roots.iter(){
+		write!(path,"{}",root).unwrap();	
+	}
+	path.push_str(".png");
+    imgbuf.save(path).unwrap();
 }
-
+//TODO make the file names based on the roots
+fn main() {
+    let ny = 50000;
+    let nx = 50000;
+	for i in 0..100{
+		make_random_im(nx, ny);
+		println!("made image {}",i+1);
+	}
+}
 #[cfg(test)]
 mod tests {
     use super::*;
